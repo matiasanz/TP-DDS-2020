783581031cac5a276578a3234d2b24bad9630125
gonzalemi <emiliano.gonzalez@hotmail.com>
2020-04-24 18:30:15 -0300

    PoC Emiliano Gonzalez

--- PruebaConcepto/src/main/java/DummyDesign.java
+++ PruebaConcepto/src/main/java/DummyDesign.java
@@ -1,7 +1,18 @@
 
+//  1) Anzorandía Matías
+//	2) Erazo Gaston
+//	3) Ferrari Guido
+//	4) González Emiliano
+//	5) Tamborini Agustín
+
 public class DummyDesign {
-	
-	public static int Integrante3() {
-		return 3;
-	}
+
+    public static int Integrante3() {
+        return 3;
+    }
+
+    public static int integrante4() {
+        return 4;
+    }
+
 }
--- PruebaConcepto/src/test/java/TestDummyDesign.java
+++ PruebaConcepto/src/test/java/TestDummyDesign.java
@@ -1,10 +1,17 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class TestDummyDesign {
 
-	@Test
-	public void testIntegrante3() {
-		assertTrue(3 == DummyDesign.Integrante3());
-	}
+    @Test
+    public void testIntegrante3() {
+        assertTrue(3 == DummyDesign.Integrante3());
+    }
+
+    @Test
+    public void testIntegrante4() {
+        assertEquals(4, DummyDesign.integrante4());
+    }
 }
