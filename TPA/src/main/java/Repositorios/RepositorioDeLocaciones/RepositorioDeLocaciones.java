1779da30c788a622ccc4f9382d7771b9134be79b
gonzalemi <emiliano.gonzalez@hotmail.com>
2020-06-14 22:21:24 -0300

    Hago revert del commit "entrega 1 - integración con Meli" para seguir trabajando en branch entrega2
    This reverts commit d5909ef6cf66283f1a6adae34b4c99c76af1b833.

--- TPA/src/main/java/Repositorios/RepositorioDeLocaciones/RepositorioDeLocaciones.java
+++
@@ -1,6 +0,0 @@
-package Repositorios.RepositorioDeLocaciones;
-import Repositorios.RepositorioDeMonedas.CodigoPais;
-
-public interface RepositorioDeLocaciones {
-    Locacion getLocacion(CodigoPais codigoPais, String codigoPostal);
-}
--- TPA/src/main/java/Repositorios/RepositorioDeMonedas/RepositorioDeMonedas.java
+++
@@ -1,7 +0,0 @@
-package Repositorios.RepositorioDeMonedas;
-import Repositorios.CodigoMoneda;
-
-public interface RepositorioDeMonedas {
-
-    Moneda getMoneda(CodigoMoneda codigoMoneda);
-}
