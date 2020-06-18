de74ed181096e4e95ed6c91f0c0dbeeb4e347695
TamboriniAgustin <tamboriniagustin00@gmail.com>
2020-06-18 03:45:37 -0300

    Tests 2da Entrega + Exceptions Repositorios (Sin asignar)

--- TPA/src/test/java/Compra/RepositorioDeMonedasMock.java
+++ TPA/src/test/java/Compra/RepositorioDeMonedasMock.java
@@ -5,8 +5,15 @@ import Moneda.CodigoMoneda;
 import Repositorios.RepositorioDeMonedas.RepositorioDeMonedas;
 
 public class RepositorioDeMonedasMock implements RepositorioDeMonedas {
-    @Override
+	String estado = "";
+	
+	@Override
     public Moneda getMoneda(CodigoMoneda codigoMoneda) {
-        return null;
+        if(estado.isEmpty()) return null;
+        else return new Moneda(CodigoMoneda.ARS, "Peso Argentino");
     }
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
 }
--- TPA/src/test/java/Compra/TestCompra.java
+++ TPA/src/test/java/Compra/TestCompra.java
@@ -1,6 +1,7 @@
 package Compra;
 import Moneda.CodigoMoneda;
 import Proveedor.Proveedor;
+import Presupuesto.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,6 +14,7 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TestCompra {
     private ArrayList<EntidadBase> entidadesBase = new ArrayList<>();
@@ -20,18 +22,61 @@ public class TestCompra {
     private Proveedor proveedor = new Proveedor(22222222, 1222222224, "Juan", "Perez", "Razon Social", null);
     private PagoEnEfectivo medioDePago = new PagoEnEfectivo();
     private Compra compra;
+    private Item item1 = new Item("Item 1", 1, BigDecimal.valueOf(50.0));
+    private Item item2 = new Item("Item 1", 1, BigDecimal.valueOf(40.5));
+    private Item item3 = new Item("Item 1", 1, BigDecimal.valueOf(9.5));
 
     @Before
     public void init() {
-
-        compra = new Compra(new RepositorioDeMonedasMock(), entidad, proveedor,  LocalDate.now(), medioDePago, CodigoMoneda.ARS, 2, null);
-        compra.agregarItem(new Item("Item 1", 1, BigDecimal.valueOf(50.0)));
-        compra.agregarItem(new Item("Item 1", 1, BigDecimal.valueOf(40.5)));
-        compra.agregarItem(new Item("Item 1", 1, BigDecimal.valueOf(9.5)));
+        compra = new Compra(new RepositorioDeMonedasMock(), entidad, proveedor,  LocalDate.now(), medioDePago, CodigoMoneda.ARS, 1, null);
+        compra.agregarItem(item1);
+        compra.agregarItem(item2);
+        compra.agregarItem(item3);
     }
 
     @Test
     public void obtenerValorTotalDeUnaCompra() {
-        assertEquals(compra.getValorTotal(),BigDecimal.valueOf(100.0));
+        assertEquals(compra.getValorTotal(), BigDecimal.valueOf(100.0));
+    }
+    
+    @Test(expected = NoHayPresupuestosSuficientesException.class)
+    public void fallaCantidadDePresupuestos() {
+    	compra.validar();
+    }
+    
+    @Test(expected = PresupuestoElegidoNoSeEncuentraEntreLosPresupuestosException.class)
+    public void presupuestoElegidoNoSeEncuentraEntreLosPresupuestosDeLaCompra() {
+    	//Agrego los presupuestos
+    	List<Item> listaItems = new ArrayList<>();
+    	listaItems.add(item1);
+    	listaItems.add(item2);
+    	listaItems.add(item3);
+    	
+    	Presupuesto presupuesto = new Presupuesto(listaItems, proveedor);
+    	//Los agrego a la compra
+    	compra.generarPresupuesto(presupuesto);
+    	//Hago la validacion
+    	compra.validar();
+    }
+    
+    @Test(expected = PresupuestoElegidoNoCumpleCriterioException.class)
+    public void presupuestoElegidoNoCumpleCriterio() {
+    	//Agrego los presupuestos
+    	List<Item> listaItems = new ArrayList<>();
+    	listaItems.add(item1);
+    	listaItems.add(item2);
+    	listaItems.add(item3);
+    	
+    	Presupuesto presupuesto1 = new Presupuesto(listaItems, proveedor);
+    	
+    	listaItems.remove(item2);
+    	
+    	Presupuesto presupuesto2 = new Presupuesto(listaItems, proveedor);
+    	//Los agrego a la compra
+    	compra.generarPresupuesto(presupuesto1);
+    	compra.generarPresupuesto(presupuesto2);
+    	compra.elegirPresupuesto(presupuesto2);
+    	//Hago la validacion
+    	compra.validar();
     }
 }
