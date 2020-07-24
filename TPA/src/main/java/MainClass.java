5992edb4563b7306f9cc72be5e9d772fba1457f8
matiasanz <matias.l.anzorandia@gmail.com>
2020-07-24 19:19:28 -0300

    fix conflictos

--- TPA/src/main/java/MainClass.java
+++ TPA/src/main/java/MainClass.java
@@ -8,11 +8,7 @@ import Fabrica.Fabrica;
 public class MainClass {
 
     public static void main(String[] args){
-<<<<<<< HEAD
     	System.out.println("\n***************************** Inicio de sesion *****************************\n");
-=======
-    	System.out.println("\n***************************** INICIO VALIDADOR DE COMPRAS *****************************\n");
->>>>>>> 5db7a77184325ca9536986299cab92542fdd69d9
     	
     	//Obtengo una organización de prueba
     	Organizacion unaOrganizacion = OrganizacionMock.getInstance();
@@ -23,11 +19,7 @@ public class MainClass {
     	BandejaDeMensajes bandejaDeMensajes = new BandejaDeMensajes(unaOrganizacion);
         bandejaDeMensajes.ejecutar();
     	
-<<<<<<< HEAD
         System.out.println("\n***************************** Fin de sesion *****************************\n");
-=======
-        System.out.println("\n***************************** FIN VALIDADOR DE COMPRAS *****************************\n");
->>>>>>> 5db7a77184325ca9536986299cab92542fdd69d9
     }
 
 }
